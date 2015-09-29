53cbbe7f801432676950275a4bd7f887ff58aa5c
Dennis Schütt <dennis.schuett@pmagentur.com>
2015-09-29 10:15:46 +0200

    added language for wordmove

--- provision-post.sh
+++ provision-post.sh
@@ -2,6 +2,8 @@
 
 yum install -y zsh vim htop nload phpmyadmin
 echo "LC_ALL=en_US.UTF-8" > /etc/environment
+echo "LANG=en_US.UTF-8" >> /etc/environment
+echo "LANGUAGE=en_US.UTF-8" >> /etc/environment
 
 runuser -l vagrant -c 'wget -qO ~/.zshrc http://git.grml.org/f/grml-etc-core/etc/zsh/zshrc'
 chsh -s $(which zsh) vagrant
